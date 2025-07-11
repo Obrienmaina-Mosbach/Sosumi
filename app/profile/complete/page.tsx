ebc52cc6bb7e3f9fb5ea76b0b0aa7e03a66f1506
Brian Nyawira <191464653+Obrienmaina-Mosbach@users.noreply.github.com>
2025-07-11 09:12:22 +0200

    @/components edit for consistency

--- app/profile/complete/page.tsx
+++ app/profile/complete/page.tsx
@@ -8,7 +8,7 @@ import { LuPencil } from 'react-icons/lu';
 import axios from 'axios'; // For fetching user data
 import { toast } from 'react-toastify'; // For notifications
 import { TailSpin } from 'react-loader-spinner'; // For loading indicator
-import BlogItem from '@/Components/BlogItem'; // Assuming you have a BlogItem component
+import BlogItem from '@/components/BlogItem'; // Assuming you have a BlogItem component
 
 interface BlogPostType {
   _id: string;
